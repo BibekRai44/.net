a64e66b9ffd654b007b73f56a226ec0850da0955
Bibekkun <bibekrai655@gmail.com>
2023-03-30 17:43:05 +0545

    switch statement program

---
+++ switch.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Enter your age ");
+int a=Convert.ToInt32(Console.ReadLine());
+
+switch (a)
+{
+    case 10:
+        Console.WriteLine("your age is 10");
+        break;
+
+    case 20:
+        Console.WriteLine("your age is 20");
+        break;
+
+    case 30:
+        Console.WriteLine("your age is 30");
+        break;
+
+    default:
+        Console.WriteLine("your age is more than 30");
+        break;
+}
+        
+Console.ReadLine();
