775dc066ab5000ad9d9ce18097bdfa9ac3f2bb42
Bibekkun <bibekrai655@gmail.com>
2023-03-30 17:52:53 +0545

    if else statement program

---
+++ user input.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter a first number");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter a seond number");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a < b)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+Console.ReadLine();
