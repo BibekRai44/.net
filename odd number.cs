f2600f4195340cf6a6bb2536e26d4f8d20440d56
Bibekkun <bibekrai655@gmail.com>
2023-03-31 18:10:12 +0545

    printing odd numbers

---
+++ odd number.cs
@@ -0,0 +1,12 @@
+﻿//Write a C# program to print the odd numbers from 1 to 99. Prints one number per line.
+
+Console.WriteLine("the odd numbers from 1 to 99 is :");
+for (int i=1; i <= 100; i++)
+{
+    if (i%2 != 0)
+    {
+        Console.WriteLine(i);
+    }
+    
+}
+Console.ReadLine();
