3d30c4933ea310c870f6e70ae0f9ebe6a6f4b41c
Bibekkun <bibekrai655@gmail.com>
2023-03-31 17:47:52 +0545

    adding letter in first and last position

---
+++ add a letter.cs
@@ -0,0 +1,9 @@
+﻿string str;
+Console.WriteLine("Enter first string:");
+str=Console.ReadLine();
+if (str.Length >= 1)
+{
+    var s = str.Substring(0, 1);
+    Console.WriteLine(s + str + s);
+}
+Console.ReadLine();
