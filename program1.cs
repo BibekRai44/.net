f6b4b54fd51b641cf66e7eb5e034637d3e387f05
Bibekkun <bibekrai655@gmail.com>
2023-04-01 14:40:04 +0545

    remove the first and last elements from a given string

---
+++ program1.cs
@@ -0,0 +1,6 @@
+﻿//Write a C# Sharp program to remove the first and last elements from a given string.
+
+Console.WriteLine("Enter a string:");
+String a=Console.ReadLine();
+Console.WriteLine(a.Substring(1, a.Length - 2));
+Console.ReadLine();
