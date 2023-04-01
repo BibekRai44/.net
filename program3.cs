e0372e56da6211602fa4d104fa2d15d6e09dcb54
Bibek kun <bibekrai655@gmail.com>
2023-04-01 16:26:20 +0545

    sum of an array

---
+++ program3.cs
@@ -0,0 +1,10 @@
+﻿//Write a C# program to print an array sum.
+
+int[] num = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+int sum = 0;
+foreach (int n in num)
+{
+    sum += n;
+}
+Console.WriteLine("sum is " + sum);
+Console.ReadLine();
