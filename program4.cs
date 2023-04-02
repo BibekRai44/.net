73c5dd1a9dd83aad73659001b2202ee07e6e0d25
Bibek kun <bibekrai655@gmail.com>
2023-04-02 07:04:36 +0545

    to check if a string starts with a specified word

---
+++ program4.cs
@@ -0,0 +1,14 @@
+﻿//Write a C# program to check if a string starts with a specified word.
+
+
+Console.WriteLine("Enter a string:");
+string a = Console.ReadLine();
+if (a.StartsWith("hello"))
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine("False");
+}
+Console.ReadLine();
